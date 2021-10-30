c1e9245f319f0eb7452f88e82e51272168d0c9aa
dchecheb <dchecheb@gmail.com>
2021-10-30 15:59:54 +0900

    fix : 주소 맵핑 수정

--- src/main/java/com/kosalaam/api/modules/chat/ChatController.java
+++ src/main/java/com/kosalaam/api/modules/chat/ChatController.java
@@ -1,14 +1,15 @@
 package com.kosalaam.api.modules.chat;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Log4j2
 public class ChatController {
 
-    @GetMapping("/chat")
+    @MessageMapping("ws/chat")
     public void chatGET() {
         log.info("@ChatController, chat GET()");
     }
