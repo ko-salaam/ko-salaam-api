0a1059329d96e3f9f3e25a25af9890ab05353783
dchecheb <dchecheb@gmail.com>
2022-02-05 11:10:34 +0900

    test: 스프링부트 통합테스트코드 추가

---
+++ src/test/java/com/kosalaam/api/KosalaamApplicationTest.java
@@ -0,0 +1,10 @@
+package com.kosalaam.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KosalaamApplicationTest {
+    @Test
+    public void contextLoads() {}
+}
